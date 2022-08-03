ba439c0c96076a39a7d5751ea0abeb29d49896b3
Andrei Similea <41330920+andrei2306@users.noreply.github.com>
2022-08-03 21:59:49 +0300

    Add files via upload

---
+++ products.js
@@ -0,0 +1,56 @@
+const products = [
+  {
+    id: 0,
+    name: "Coffee 1",
+    price: 29.99,
+    instock: 100,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "./img/Coffee1.png",
+  },
+  {
+    id: 1,
+    name: "Coffee 2",
+    price: 24.99,
+    instock: 43,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "./img/Coffee2.png",
+  },
+  {
+    id: 2,
+    name: "Coffee 3",
+    price: 19.99,
+    instock: 10,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "./img/Coffee3.png",
+  },
+  {
+    id: 0,
+    name: "Coffee 4",
+    price: 29.99,
+    instock: 100,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "./img/Coffee1.png",
+  },
+  {
+    id: 1,
+    name: "Coffee 5",
+    price: 24.99,
+    instock: 43,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "./img/Coffee2.png",
+  },
+  {
+    id: 2,
+    name: "Coffee 6",
+    price: 19.99,
+    instock: 10,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "./img/Coffee3.png",
+  },
+];
